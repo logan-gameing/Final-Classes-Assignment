ab4e351fc5639cfffa199f5a487f7a52a98e3314
logan-gameing <Logan.Jones1@edu.sait.ca>
2024-12-06 10:23:00 -0700

    Update program.py

--- program.py
+++ program.py
@@ -1,4 +1,6 @@
 #Logan
+import os
+
 class DoctorManager():
     def __init__(self):
         doctors = []
@@ -6,4 +8,22 @@ class DoctorManager():
         #READ DOCDOR
 
     def format_dr_info():
-        
+        print("EMPY")
+
+    def enter_dr_info():
+        print("EMPY")
+
+    def read_doctors_file():
+        print("EMPY")
+
+        f_obj = open('/doctors.txt', 'r')
+
+        line = f_obj.readline()
+        while line != '':
+            line = line.rstrip()
+            print(line)
+            line = f_obj.readline()
+            f_obj.close()
+
+a = DoctorManager()
+a.read_doctors_file
